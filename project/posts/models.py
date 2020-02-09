b110d57db96c525e0277ca8b3c31897b383f494f
Vlad <vlad.uralsk@gmail.com>
2020-02-09 17:42:20 +0400

    Модель post

--- project/posts/models.py
+++ project/posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Заголовок')
+    body = models.TextField(blank=True, db_index=True, verbose_name='Текст статьи')
+    date_create = models.DateTimeField(auto_created=True, verbose_name='Дата создания')
+    date_pub = models.DateTimeField(auto_now=True, verbose_name='Дата публикации')
