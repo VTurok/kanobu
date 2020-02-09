291759a78dafb581c5ae5d785d0d40e00ae04af7
Vlad <vlad.uralsk@gmail.com>
2020-02-09 20:28:53 +0400

    Расширение моделей post и news, добавление автора материала

--- project/news/models.py
+++ project/news/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # Create your models here.
 class News(models.Model):
@@ -7,3 +8,4 @@ class News(models.Model):
     body = models.TextField(blank=True, db_index=True, verbose_name='Текст новости')
     date_create = models.DateTimeField(auto_created=True, verbose_name='Дата создания')
     date_pub = models.DateTimeField(auto_now=True, verbose_name='Дата публикации')
+    author = models.ForeignKey(User, verbose_name='Автор', on_delete=models.CASCADE)
--- project/posts/models.py
+++ project/posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # Create your models here.
 class Post(models.Model):
@@ -7,3 +8,4 @@ class Post(models.Model):
     body = models.TextField(blank=True, db_index=True, verbose_name='Текст статьи')
     date_create = models.DateTimeField(auto_created=True, verbose_name='Дата создания')
     date_pub = models.DateTimeField(auto_now=True, verbose_name='Дата публикации')
+    author = models.ForeignKey(User, verbose_name='Автор', on_delete=models.CASCADE)
