d1049a82f27ea651bbf1b2bc35b2b10c4575aa65
Vlad <vlad.uralsk@gmail.com>
2020-02-09 17:43:51 +0400

    Модель news

--- project/news/models.py
+++ project/news/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Заголовок')
+    body = models.TextField(blank=True, db_index=True, verbose_name='Текст новости')
+    date_create = models.DateTimeField(auto_created=True, verbose_name='Дата создания')
+    date_pub = models.DateTimeField(auto_now=True, verbose_name='Дата публикации')
